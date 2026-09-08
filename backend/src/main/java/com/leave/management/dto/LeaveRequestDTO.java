dbf32f28844fff0a73014356838809829e8e16e1
Sachin M <sachinm.cs23@bitsathy.ac.in>
2026-09-08 18:33:34 +0530

    refactor: remove unused import in LeaveRequestDTO

--- backend/src/main/java/com/leave/management/dto/LeaveRequestDTO.java
+++ backend/src/main/java/com/leave/management/dto/LeaveRequestDTO.java
@@ -1,6 +1,5 @@
 package com.leave.management.dto;
 
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
